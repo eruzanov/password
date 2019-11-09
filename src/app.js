b2f8a2a7d28ecdaf08d3a5a155c1f426e22cac5c
EugeneRuzanov <eugene.ruzanov@gmail.com>
2019-11-09 22:26:37 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import generator from "password-generator";
+import "./styles.css";
+
+const input = document.querySelector("input");
+
+function copy() {
+  input.select();
+  input.setSelectionRange(0, 99999); //For mobile devices
+  document.execCommand("copy");
+  input.blur(); 
+}
+
+function generate() {
+  input.value = generator(12, false);
+}
+
+generate();
+document.querySelector(".generate").addEventListener("click", generate);
+document.querySelector(".copy").addEventListener("click", copy);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/app.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "generator a password",
+      template: "src/index.html"
+    })
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
