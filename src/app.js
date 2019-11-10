d20c9ea6862c3770194bc476336eaeb2c1f0457c
EugeneRuzanov <eugene.ruzanov@gmail.com>
2019-11-10 20:14:07 +0300

    added params

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
 import generator from "password-generator";
 import "./styles.css";
 
-const input = document.querySelector("input");
+const form = document.querySelector("form");
 
 function copy() {
-  input.select();
-  input.setSelectionRange(0, 99999); //For mobile devices
+  form.password.select();
+  form.password.setSelectionRange(0, 99999); //For mobile devices
   document.execCommand("copy");
-  input.blur();
+  form.password.blur();
 }
 
 function generate() {
-  input.value = generator(12, false);
+  const checkboxes = [];
+  if (form.digit.checked) checkboxes.push("\\d");
+  if (form.alphanumeric.checked) checkboxes.push("\\w");
+  if (form.nonWord.checked) checkboxes.push("\\W");
+  const pattern = checkboxes.length
+    ? new RegExp("[" + checkboxes.join("") + "]")
+    : null;
+  form.password.value = generator(12, false, pattern);
 }
 
+form.generate.addEventListener("click", generate);
+form.copy.addEventListener("click", copy);
+form.digit.addEventListener("change", generate);
+form.alphanumeric.addEventListener("change", generate);
+form.nonWord.addEventListener("change", generate);
+
+form.digit.checked = true;
+form.alphanumeric.checked = true;
+form.nonWord.checked = true;
+
 generate();
-document.querySelector(".generate").addEventListener("click", generate);
-document.querySelector(".copy").addEventListener("click", copy);
